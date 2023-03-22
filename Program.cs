6b257295147122e90f7af2deffbd548f9cd473da
Nikita Matveev <nikitamatveev92@gmail.com>
2023-03-22 22:13:10 +1000

    Code done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] initialArray = { "Hello", "-2", "world", ":-)", "124", "Denmark", "Moscow", "+" };
+int count = 0;
+int findLength = 3;
+
+for (int index = 0; index < initialArray.Length; index++)
+{
+  if (initialArray[index].Length <= findLength)
+  {
+    count++;
+  }
+}
+
+if (count != 0)
+{
+  string[] finalArray = new string[count];
+  int j = 0;
+  for (int index = 0; index < initialArray.Length; index++)
+  {
+    if (initialArray[index].Length <= findLength)
+    {
+      finalArray[j] = initialArray[index];
+      j++;
+    }
+  }
+
+  for (int index = 0; index < count; index++)
+  {
+    if (count == 1)
+    {
+      Console.WriteLine($"[{finalArray[index]}]");
+    }
+    else if (index == 0)
+    {
+      Console.Write($"[{finalArray[index]}, ");
+    }
+    else if (index == count - 1)
+    {
+      Console.WriteLine($"{finalArray[index]}]");
+    }
+    else
+    {
+      Console.Write($"{finalArray[index]}, ");
+    }
+  }
+  
+}
+else
+{
+  Console.WriteLine("[  ]");
+}
